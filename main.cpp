9a7fa1d0bf1d9f5f6bd0b344201562b1144a4aa5
Krystian Chachuła <krystiancha@gmail.com>
2019-04-25 21:20:47 +0200

    add Simple-Web-Server dependency

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <boost/program_options.hpp>
+#include <server_http.hpp>
+
+using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
 
 int main(int argc, char *argv[]) {
     namespace po = boost::program_options;
@@ -23,6 +26,28 @@ int main(int argc, char *argv[]) {
     } else {
         std::cout << "Hello, unknown.\n";
     }
+
+    HttpServer server;
+    server.config.address = "127.0.0.1";
+    server.config.port = 8081;
+
+    server.resource["^/$"]["POST"] = [](
+        std::shared_ptr<HttpServer::Response> response,
+        std::shared_ptr<HttpServer::Request> request
+    ) {
+        response->write("Hello.");
+    };
+
+    server.on_error = [](std::shared_ptr<HttpServer::Request> /*request*/, const SimpleWeb::error_code & ec) {
+        std::cerr << ec.message() << std::endl;
+    };
+
+    std::thread server_thread([&server]() {
+        server.start();
+        std::cout << "The server is listening on " << server.config.address << ":" << server.config.port << std::endl;
+    });
+
+    server_thread.join();
 
     return 0;
 }
