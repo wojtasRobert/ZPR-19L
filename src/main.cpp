b2bcbade8336004ecf4ba989ceefce5cf6d2445c
Krystian Chachuła <krystiancha@gmail.com>
2019-06-07 08:59:24 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <boost/program_options.hpp>
 #include <server_http.hpp>
 
 #include <resmond/clientinterface.hpp>
@@ -7,31 +6,9 @@
 #include <resmond/resourcemonitor.hpp>
 #include <resmond/limitmanager.hpp>
 
-namespace po = boost::program_options;
-
 int main(int argc, char *argv[]) {
 
     using namespace std::chrono_literals;
-
-    po::options_description desc("Allowed options");
-    desc.add_options()
-        ("help", "produce help message")
-        ("name", po::value<std::string>(), "set name");
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-        std::cout << desc << "\n";
-        return 1;
-    }
-
-    if (vm.count("name")) {
-        std::cout << "Hello, " << vm["name"].as<std::string>() << ".\n";
-    } else {
-        std::cout << "Hello, unknown.\n";
-    }
 
     auto limitManager = std::make_shared<resmond::LimitManager>();
     auto emailSender = std::make_shared<resmond::EmailSender>("../script/mailer", 5min);
